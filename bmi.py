a3eb339521c13624a5c76811c4860d8d0f1edf86
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-06-28 15:01:16 +0800

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,11 @@
+#BMI = kg / m2
+
+weight = #how to input
+height = #how to input
+
+BMI = #how to calculate BMI?
+
+if BMI < 18:
+    print("Underweight")
+#BMI between 18 and 24, that's normal
+#BMI over 24, that's overweight
